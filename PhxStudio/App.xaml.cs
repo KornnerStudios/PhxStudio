1d6a2d7bc3f6f42f8b27b5a0f8907bf4e211b59d
kornman00 <kornman00@gmail.com>
2017-04-08 11:15:26 -0500

    Added StartupModule and PhxLogo.

--- PhxStudio/App.xaml.cs
+++ PhxStudio/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace PhxStudio
 {
@@ -13,5 +16,40 @@ namespace PhxStudio
 	/// </summary>
 	public partial class App : Application
 	{
-	}
+		public static RenderTargetBitmap AppIconBitmap { get; private set; }
+
+		protected override void OnActivated(EventArgs e)
+		{
+			base.OnActivated(e);
+
+			RenderAppIconBitmap();
+		}
+
+		private void RenderAppIconBitmap()
+		{
+			if (AppIconBitmap != null)
+				return;
+
+			var grid = (Grid)FindResource("PhxLogoGrid");
+			if (grid == null)
+				throw new ArgumentException("Failed to find logo Grid", "PhxLogoGrid");
+
+			var viewbox = new Viewbox();
+			viewbox.Child = grid;
+			viewbox.Measure(new Size(512, 512));
+			viewbox.Arrange(new Rect(0, 0, 512, 512));
+			viewbox.UpdateLayout();
+
+			var viewbox_ps = PresentationSource.FromVisual(viewbox);
+			double dpiX = 96.0, dpiY = 96.0;
+			if (viewbox_ps != null)
+			{
+				dpiX *= viewbox_ps.CompositionTarget.TransformToDevice.M11;
+				dpiY *= viewbox_ps.CompositionTarget.TransformToDevice.M22;
+			}
+
+			AppIconBitmap = new RenderTargetBitmap((int)viewbox.ActualWidth, (int)viewbox.ActualHeight, dpiX, dpiY, PixelFormats.Pbgra32);
+			AppIconBitmap.Render(viewbox);
+		}
+	};
 }
---
+++ PhxStudio/Modules/Startup/StartupModule.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Gemini.Framework;
+using Gemini.Framework.Menus;
+
+namespace PhxStudio.Modules.Startup
+{
+	[Export(typeof(IModule))]
+	sealed class StartupModule : ModuleBase
+	{
+		public override void Initialize()
+		{
+			MainWindow.Title = "PhxStudio";
+			MainWindow.Icon = App.AppIconBitmap;
+		}
+	};
+}
