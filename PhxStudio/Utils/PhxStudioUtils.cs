eb9e11a98b3f3ba82366dbdd9ca9b7810139732c
kornman00 <kornman00@gmail.com>
2026-07-07 18:30:34 -0700

    Use explicit enum constraint in PhxStudio utility helper

--- PhxStudio/Utils/PhxStudioUtils.cs
+++ PhxStudio/Utils/PhxStudioUtils.cs
@@ -40,7 +40,7 @@ namespace PhxStudio
 			, ref TEnum field, TEnum value
 			, bool overrideChecks = false
 			, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
-			where TEnum : struct, IComparable, IFormattable, IConvertible
+			where TEnum : struct, Enum, IComparable, IFormattable, IConvertible
 		{
 			if (obj == null)
 				return false;
